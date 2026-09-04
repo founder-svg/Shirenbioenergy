b091b60c999d0fc254ba33b56584a5e4f4ba53f6
founder-svg <founder@wixfox.com>
2026-09-04 19:10:48 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { fileURLToPath, URL } from 'url'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: './',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    assetsInlineLimit: 0, // Never inline images into JS/HTML bundle. Emit every image as a separate file under 1MB!
+  },
+})
