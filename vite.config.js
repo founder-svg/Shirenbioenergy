f92eb9066c8b18acba6389ca0f29f6a6afce7fc9
Akash <akash@wixfox.com>
2026-09-04 22:13:29 +0530

    Add compiled production bundle to root and dist for instant static deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { fileURLToPath, URL } from 'url'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Shirenbioenergy/',
+  base: './',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
